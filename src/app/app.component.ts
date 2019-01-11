2c9ce315b15c606c7d1e2201c91ab665c55b2eee
ym1997 <ym1997@localhost.localdomain>
2019-01-11 22:29:17 +0900

    Section 2 lecture 24

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { Post } from './post/post.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mean-imageforum';
+  storedPosts: Post[] = [];
+
+  onPostAdded(post) {
+    this.storedPosts.push(post);
+  }
 }
--- src/app/post/post-list/post-list.component.ts
+++ src/app/post/post-list/post-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post-list',
@@ -6,9 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent {
-  posts = [
-    {title: 'First Post', content: 'This is the first post'},
-    {title: 'Second Post', content: 'This is the second post'},
-    {title: 'Third Post', content: 'This is the third post'},
-  ];
+  // posts = [
+  //   {title: 'First Post', content: 'This is the first post'},
+  //   {title: 'Second Post', content: 'This is the second post'},
+  //   {title: 'Third Post', content: 'This is the third post'},
+  // ];
+
+  @Input() posts: Post[] = [];
 }
